582a7bd958299a81bfb4c9158e6d62ac7c6da6e0
wiwindos <zmzhenya@yandex.ru>
2018-11-12 20:59:45 +0000

    1

--- core.php
+++ core.php
@@ -1,4 +1,4 @@
-<?php
+<php
 include_once('./core/GetDataFromChat.php');
 include_once('./core/Methods.php');
 include_once('./core/DefaultSettings.php');
@@ -6,13 +6,10 @@ include_once('./core/DefaultSettings.php');
 $stream = webHook::getData();					//подрубаем входящие данные
 //$logs = Logs::setLog();							//подрубаем логи
 
-session_id(Message::getChatId());	//сессия
-@session_start();
+//session_id(Message::getChatId());	//сессия
+//@session_start();
 
 //if ( isset($data[message][entities]) ){}
 
 /////////////////////////////////////// обновление BD, большой кейс (проверка состояния по сессии, инлайн, сообщения с команд, кнопок и тд)
-
-
 SendMessage::send("DAROVA");
-?>
