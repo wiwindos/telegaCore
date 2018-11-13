9428583c905d7d92ab6b58b6c6ec9c4a1ce02687
wiwindos <zmzhenya@yandex.ru>
2018-11-13 17:50:39 +0000

    1

--- core.php
+++ core.php
@@ -6,17 +6,17 @@ include_once (dirname(__FILE__).'/core/DefaultSettings.php');
 $stream = webHook::getData();					//подрубаем входящие данные
 //$logs = Logs::setLog();							//подрубаем логи
 
-session_id(Message::getChatId());	//сессия
-@session_start();
+//session_id(Message::getChatId());	//сессия
+//@session_start();
 
 /*if ( isset($data[message][entities]) ){}*/
 
 /*/////////////////////////////////////// обновление BD, большой кейс (проверка состояния по сессии, инлайн, сообщения с команд, кнопок и тд)*/
 
-if(Message::getText() == "1"){
+/*if(Message::getText() == "1"){
     $msg = "Здравствуй, ".Message::getFromFirstName()."!";
     SendMessage::send($msg);
-}
+}*/
 $a = Message::getText();
 SendMessage::send($a);
 ?>
