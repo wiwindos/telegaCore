2292d4045e1aa143dbe709af8a749a5b12eb108e
wiwindos <zmzhenya@yandex.ru>
2018-11-13 09:03:05 +0000

    1

--- core/DefaultSettings.php
+++ core/DefaultSettings.php
@@ -1,9 +1,9 @@
 <?php
 final class DefaultSettings {
     //https://api.telegram.org/bot410244451:AAGrJ83kT1E0ohx_lgLPmwM/setWebhook?url=https://xxx.xx/bot/.php
-    //static protected $token = getenv('TOKEN');
+    static protected $token = getenv('TOKEN');
     //static protected $token = $_ENV['TOKEN'];
-	static protected $token = "373935436:AAEe5bcAdyXz4vBv6olwCFs0RyuTA3trlTE";						//токен
+	//static protected $token = "373935436:AAEe5bcAdyXz4vBv6olwCFs0RyuTA3trlTE";						//токен
 	
 	//static protected $user = 'user';														//подключение к бд
 	//static protected $password = 'password';
